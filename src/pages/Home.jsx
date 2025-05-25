b5b7151c535242d66ca7034e5de92007e697bf78
soul017 <sengar017lokesh@gmail.com>
2025-05-25 13:57:15 +0530

    Contact section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { ProjectSection } from "../components/ProjectSection";
+import { ContactSection } from "../components/ContactSection";
 export const Home = () => {
   return (
     <div className="min-h-screen  bg-background text-foreground overflow-x-hidden">
@@ -20,6 +21,7 @@ export const Home = () => {
         <AboutSection/>
         <SkillsSection/>
         <ProjectSection/>
+        <ContactSection/>
       </main>
 
       {/* Footer */}
